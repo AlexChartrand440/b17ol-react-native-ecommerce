eeb69272b38f2bdd91b1cc5a5ee6bcaccf0a83ab
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-10-27 00:33:58 +0700

    slicing login screen

---
+++ src/screens/Login.js
@@ -0,0 +1,93 @@
+/* eslint-disable prettier/prettier */
+import React, { useState } from 'react';
+import { TouchableOpacity, StyleSheet } from 'react-native';
+import {
+  Container,
+  Content,
+  Form,
+  Item,
+  Label,
+  Input,
+  Text,
+  Button,
+  Card,
+  CardItem,
+  Body,
+} from 'native-base';
+
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function forgot() {
+    navigation.navigate('Forgot');
+  }
+
+  function doLogin() {
+    navigation.navigate('Home');
+  }
+
+  return (
+    <Container>
+      <Content padder style={styles.parent}>
+        <Text style={styles.header}>
+          Login
+        </Text>
+        <Form>
+          <Card>
+            <CardItem>
+              <Body>
+                <Item floatingLabel>
+                  <Label>Email</Label>
+                  <Input onChangeText={text => setEmail(text)} />
+                </Item>
+              </Body>
+            </CardItem>
+          </Card>
+          <Card>
+            <CardItem>
+              <Body>
+                <Item floatingLabel>
+                  <Label>Password</Label>
+                  <Input secureTextEntry={true} onChangeText={text => setPassword(text)} />
+                </Item>
+              </Body>
+            </CardItem>
+          </Card>
+        </Form>
+        <TouchableOpacity onPress={forgot} style={styles.login}>
+          <Text style={styles.loginLink}>
+            Forgot your password?
+          </Text>
+        </TouchableOpacity>
+        <Button
+          rounded
+          block
+          success
+          onPress={doLogin}
+          disabled={(email === '' || password === '') ? true : false}
+        >
+          <Text>Login</Text>
+        </Button>
+      </Content>
+    </Container>
+  );
+}
+
+const styles = StyleSheet.create({
+  parent: {
+    backgroundColor: '#F9F9F9',
+  },
+  header: {
+    fontSize: 34,
+    fontWeight: 'bold',
+    marginBottom: 70,
+  },
+  loginLink: {
+    marginTop: 16,
+    marginBottom: 28,
+  },
+  login: {
+    alignItems: 'flex-end',
+  },
+});
