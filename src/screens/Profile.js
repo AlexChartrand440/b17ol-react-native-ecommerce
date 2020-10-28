aa9a9804ab9a515d767f57de9eb5bb4ac901daff
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-10-29 01:40:44 +0700

    slicing profile screen

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,19 +1,60 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import {
   Container,
   Content,
   Text,
+  Thumbnail,
+  List,
+  ListItem,
+  Left,
+  Right,
+  Icon,
 } from 'native-base';
+
+import User from '../assets/img/avatar.png';
 
 export default function Profile() {
   return (
     <Container style={styles.parent}>
       <Content padder>
-        <Text>
-          This is Profile Section
+        <Text style={styles.header}>
+          My Profile
         </Text>
+        <View style={styles.avatar}>
+          <Thumbnail source={User} />
+          <View style={styles.personalInfo}>
+            <Text style={styles.fullname}>Matilda Brown</Text>
+            <Text style={styles.email}>matildabrown@mail.com</Text>
+          </View>
+        </View>
+        <List>
+          <ListItem>
+            <Left>
+              <Text style={styles.listItemText}>My Orders</Text>
+            </Left>
+            <Right>
+              <Icon type="MaterialIcons" name="arrow-forward" />
+            </Right>
+          </ListItem>
+          <ListItem>
+            <Left>
+              <Text style={styles.listItemText}>Shipping Addresses</Text>
+            </Left>
+            <Right>
+              <Icon type="MaterialIcons" name="arrow-forward" />
+            </Right>
+          </ListItem>
+          <ListItem>
+            <Left>
+              <Text style={styles.listItemText}>Settings</Text>
+            </Left>
+            <Right>
+              <Icon type="MaterialIcons" name="arrow-forward" />
+            </Right>
+          </ListItem>
+        </List>
       </Content>
     </Container>
   );
@@ -22,5 +63,29 @@ export default function Profile() {
 const styles = StyleSheet.create({
   parent: {
     backgroundColor: '#F9F9F9',
+  },
+  header: {
+    fontSize: 34,
+    fontWeight: 'bold',
+    marginBottom: 24,
+  },
+  avatar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 40,
+  },
+  personalInfo: {
+    marginLeft: 16,
+  },
+  fullname: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  email: {
+    fontSize: 14,
+  },
+  listItemText: {
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
