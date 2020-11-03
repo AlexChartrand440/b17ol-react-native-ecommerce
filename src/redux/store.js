0910ed08c0de6e911216fa80226b0038376aafb7
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-03 07:52:51 +0700

    using redux to connect the backend

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers/index';
+import logger from 'redux-logger';
+import promiseMiddleware from 'redux-promise-middleware';
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(promiseMiddleware, logger),
+);
+
+export default store;
