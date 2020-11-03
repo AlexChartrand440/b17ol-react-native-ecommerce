0477abcf905d2a74be6f8c94152b8e74f7faad4e
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-03 09:02:03 +0700

    using redux-persist to save the redux store to AsyncStorage

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
-// import store
-import store from './src/redux/store';
+// import store & persistor
+import {store, persistor} from './src/redux/store';
 
 import Main from './src/screens/Main';
 
@@ -10,7 +11,9 @@ export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Main />
+        <PersistGate loading={null} persistor={persistor}>
+          <Main />
+        </PersistGate>
       </Provider>
     );
   }
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,27 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/index';
 import logger from 'redux-logger';
 import promiseMiddleware from 'redux-promise-middleware';
+import AsyncStorage from '@react-native-community/async-storage';
+import { persistStore, persistReducer } from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: [
+    'auth',
+  ],
+  // blacklist: [
+  //   'counterReducer',
+  // ],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   applyMiddleware(promiseMiddleware, logger),
 );
 
-export default store;
+let persistor = persistStore(store);
+
+export { store, persistor };
