e5875e2ca11a9735f44c87fbdbf7eeadbdc0d764
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-14 11:59:10 +0700

    add form validation using formik+yup on signup form

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,6 +1,5 @@
-/* eslint-disable prettier/prettier */
-import React, {useState} from 'react';
-import { TouchableOpacity, StyleSheet } from 'react-native';
+import React from 'react';
+import {TouchableOpacity, StyleSheet, View} from 'react-native';
 import {
   Container,
   Content,
@@ -14,11 +13,20 @@ import {
   CardItem,
   Body,
 } from 'native-base';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
 
-export default function Signup({ navigation }) {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+export default function Signup({navigation}) {
+  const schema = Yup.object().shape({
+    name: Yup.string().max(50, 'Max 50 character').required('Required field'),
+    email: Yup.string()
+      .email('Invalid email')
+      .max(50, 'Max 50 character')
+      .required('Required field'),
+    password: Yup.string()
+      .min(6, 'Password required 6 character')
+      .required('Required field'),
+  });
 
   function login() {
     navigation.navigate('Login');
@@ -31,55 +39,90 @@ export default function Signup({ navigation }) {
   return (
     <Container>
       <Content padder style={styles.parent}>
-        <Text style={styles.header}>
-          Sign up
-        </Text>
-        <Form>
-          <Card>
-            <CardItem>
-              <Body>
-                <Item floatingLabel>
-                  <Label>Name</Label>
-                  <Input onChangeText={text => setName(text)} />
-                </Item>
-              </Body>
-            </CardItem>
-          </Card>
-          <Card>
-            <CardItem>
-              <Body>
-                <Item floatingLabel>
-                  <Label>Email</Label>
-                  <Input onChangeText={text => setEmail(text)} />
-                </Item>
-              </Body>
-            </CardItem>
-          </Card>
-          <Card>
-            <CardItem>
-              <Body>
-                <Item floatingLabel>
-                  <Label>Password</Label>
-                  <Input secureTextEntry={true} onChangeText={text => setPassword(text)} />
-                </Item>
-              </Body>
-            </CardItem>
-          </Card>
-        </Form>
-        <TouchableOpacity onPress={login} style={styles.login}>
-          <Text style={styles.loginLink}>
-            Already have an account?
-          </Text>
-        </TouchableOpacity>
-        <Button
-          rounded
-          block
-          success
-          onPress={doSignup}
-          disabled={(name === '' || email === '' || password === '') ? true : false}
-        >
-          <Text>Sign Up</Text>
-        </Button>
+        <Text style={styles.header}>Sign up</Text>
+
+        <Formik
+          initialValues={{
+            name: '',
+            email: '',
+            password: '',
+          }}
+          validationSchema={schema}
+          onSubmit={(values) => console.log(values)}>
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            values,
+            isValid,
+            isSubmitting,
+            touched,
+            errors,
+          }) => (
+            <View>
+              <Form>
+                <Card>
+                  <CardItem>
+                    <Body>
+                      <Item floatingLabel>
+                        <Label>Name</Label>
+                        <Input
+                          onChangeText={handleChange('name')}
+                          onBlur={handleBlur('name')}
+                          value={values.name}
+                        />
+                      </Item>
+                      {touched.name && errors.name && (
+                        <Text style={styles.error}>{errors.name}</Text>
+                      )}
+                    </Body>
+                  </CardItem>
+                </Card>
+                <Card>
+                  <CardItem>
+                    <Body>
+                      <Item floatingLabel>
+                        <Label>Email</Label>
+                        <Input
+                          onChangeText={handleChange('email')}
+                          onBlur={handleBlur('email')}
+                          value={values.email}
+                        />
+                      </Item>
+                      {touched.email && errors.email && (
+                        <Text style={styles.error}>{errors.email}</Text>
+                      )}
+                    </Body>
+                  </CardItem>
+                </Card>
+                <Card>
+                  <CardItem>
+                    <Body>
+                      <Item floatingLabel>
+                        <Label>Password</Label>
+                        <Input
+                          secureTextEntry={true}
+                          onChangeText={handleChange('password')}
+                          onBlur={handleBlur('password')}
+                          value={values.password}
+                        />
+                      </Item>
+                      {touched.password && errors.password && (
+                        <Text style={styles.error}>{errors.password}</Text>
+                      )}
+                    </Body>
+                  </CardItem>
+                </Card>
+              </Form>
+              <TouchableOpacity onPress={login} style={styles.login}>
+                <Text style={styles.loginLink}>Already have an account?</Text>
+              </TouchableOpacity>
+              <Button rounded block success onPress={handleSubmit}>
+                <Text>Sign Up</Text>
+              </Button>
+            </View>
+          )}
+        </Formik>
       </Content>
     </Container>
   );
@@ -100,5 +143,9 @@ const styles = StyleSheet.create({
   },
   login: {
     alignItems: 'flex-end',
+  },
+  error: {
+    fontSize: 12,
+    color: 'red',
   },
 });
