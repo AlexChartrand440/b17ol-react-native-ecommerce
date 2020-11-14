8ce2e9137bd1c23a64275e5c42caf92f9535991d
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-14 14:09:20 +0700

    add app splashscreen using react-native-splash-screen

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
+import SplashScreen from 'react-native-splash-screen';
 
 // import store & persistor
 import {store, persistor} from './src/redux/store';
 
 import Main from './src/screens/Main';
 
-export default class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Main />
-        </PersistGate>
-      </Provider>
-    );
-  }
+export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Main />
+      </PersistGate>
+    </Provider>
+  );
 }
